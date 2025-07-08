dd5e055f9d5cf81f7fd68f01c82f4827cd1e6557
Arthur Medeiros Pinheiro <20231011110020@ifrn.local>
2025-07-08 15:33:25 -0300

    Exercicios de Banco de dados pt.2

--- Aulabd 2/AulasBD_v2/app/views.py
+++ Aulabd 2/AulasBD_v2/app/views.py
@@ -7,7 +7,7 @@ def obter_conexao():
     # define os parametros de conexao
     driver   = '{ODBC Driver 17 for SQL Server}'
     servidor = '.\SQLEXPRESS'
-    banco    = 'Aulas_DB'
+    banco    = 'Aulas_BD'
     usuario  = 'sa'
     senha    = 'Senha@123' # poder ser também: "senha", "senha@123", "Senha@123"
 
@@ -79,6 +79,85 @@ def exercicio_2(request):
             INNER JOIN Turma tur ON tur.id = alun.turma_id
 
             ORDER BY tur.nome, alun.nome
+        '''
+        
+        # usa o cursor para executar o SQL
+        cursor.execute(sql)
+        # obtem todos os registros retornados
+        registros = cursor.fetchall()
+
+        # define a pagina a ser carregada, adicionando os registros das tabelas 
+        return render(request, template, context={'registros': registros})
+    
+    # se ocorreu algunm erro, insere a mensagem para ser exibida no contexto da página 
+    except Exception as err:
+        return render(request, template, context={'ERRO': err})
+
+
+def exercicio_3(request):
+    # define a página HTML (template) que deverá será carregada
+    template = 'exercicio_3.html'
+    try:
+        # obtem a conexao com o BD
+        conexao = obter_conexao()
+
+        # define um cursor para executar comandos SQL
+        cursor = conexao.cursor()
+
+        # define o comando SQL que será executado
+        sql = '''
+            SELECT
+                est.nome AS estado,
+                cid.nome AS cidade,
+                bai.nome AS bairro
+            FROM Estado est
+            INNER JOIN Cidade cid ON est.sigla = cid.estado_id
+            INNER JOIN Bairro bai ON cid.id = bai.cidade_id
+            ORDER BY est.nome, cid.nome, bai.nome
+        '''
+        
+        # usa o cursor para executar o SQL
+        cursor.execute(sql)
+        # obtem todos os registros retornados
+        registros = cursor.fetchall()
+
+        # define a pagina a ser carregada, adicionando os registros das tabelas 
+        return render(request, template, context={'registros': registros})
+    
+    # se ocorreu algunm erro, insere a mensagem para ser exibida no contexto da página 
+    except Exception as err:
+        return render(request, template, context={'ERRO': err})
+
+
+
+def exercicio_4(request):
+    # define a página HTML (template) que deverá será carregada
+    template = 'exercicio_4.html'
+    try:
+        # obtem a conexao com o BD
+        conexao = obter_conexao()
+
+        # define um cursor para executar comandos SQL
+        cursor = conexao.cursor()
+
+        # define o comando SQL que será executado
+        sql = '''
+            SELECT
+                fab.descricao,
+                mod.descricao,
+                car.ano_fabricacao,
+                car.cor,
+                car.placa,
+                car.preco,
+                cat.descricao
+
+            FROM Carro car
+            INNER JOIN Modelo mod ON mod.id = car.modelo_id
+            INNER JOIN Fabricante fab ON fab.id = mod.fabricante_id
+            INNER JOIN Categoria cat ON cat.id = car.categoria_id
+          
+
+            ORDER BY fab.descricao, mod.descricao, car.ano_fabricacao
         '''
         
         # usa o cursor para executar o SQL
